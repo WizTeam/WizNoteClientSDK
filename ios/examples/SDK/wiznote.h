f6cdd63f7e70c8db7e46c3b488fbeeb8038df822
wizwzz <wuzhuanzhuanty@gmail.com>
2019-01-15 18:48:10 +0800

    增加接口，可以单独拉起编辑、阅读、笔记本列表界面

--- ios/examples/SDK/wiznote.h
+++ ios/examples/SDK/wiznote.h
@@ -10,5 +10,7 @@
 
 @interface WizNote : NSObject
 + (void) setup:(NSDictionary*)options;
-+ (void) launchWizNote:(UIViewController *)parentViewController options:(NSDictionary *)options customActionBlock:(id)customActionBlock updateCookiesBlock:(id)updateCookiesBlock shareNoteCallbackBlock:(id)shareNoteCallbackBlock;
++ (void) launchWizNote:(UIViewController *)parentViewController options:(NSDictionary *)options customActionBlock:(id)customActionBlock updateCookiesBlock:(id)updateCookiesBlock shareNoteCallbackBlock:(id)shareNoteCallbackBlock delegate:(id)object;
++ (void) getDocumentsListBy:(NSString*)appId objectId:(NSString*)objectId userOptions:(NSDictionary*)options completeBlock:(void(^)(NSArray* documents))block;
++ (void) getDocumentsListByCategory:(NSString*)category userOptions:(NSDictionary*)options first:(int64_t)first count:(int64_t)count completeBlock:(void(^)(NSArray* documents))block;
 @end
